d13a4509205bf9e4cd1e0a02d60282e4a4974963
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-02-04 00:02:05 +0100

    generation des biomes

--- Assets/Scripts/MapBase.cs
+++ Assets/Scripts/MapBase.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class MapBase : MonoBehaviour
 {
-
     [SerializeField] private GameObject[] tilePrefabs;
     [SerializeField] private Vector3 offset;
     [SerializeField] private int width;
     [SerializeField] private int height;
     [SerializeField] private float space;
+    private float noiseSpacing = 5f;
 
 
     public enum TileType : int
     {
-        Grass = 0
+        Grass = 0,
+        Field = 1
     }
 
     private TileType[,] tiles;
@@ -34,13 +35,22 @@ public class MapBase : MonoBehaviour
 
     private void initMap()
     {
-        
+        float randomOffset = Random.Range(-100f, 100f); 
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                tilesObject[i, j] = Instantiate(tilePrefabs[(int)tiles[i,j]]);
-                tilesObject[i, j].transform.position = offset + new Vector3(i*space + space/2*(j%2), 0, j * space * 5.0f/6.0f);
+                float noiseTileType = Mathf.PerlinNoise(randomOffset + ((float)i)/noiseSpacing , randomOffset + ((float)j) / noiseSpacing);
+                if(noiseTileType > 0.7f)
+                {
+                    tiles[i, j] = TileType.Field;
+                }
+                else
+                {
+                    tiles[i, j] = TileType.Grass;
+                }
+                tilesObject[i, j] = Instantiate(tilePrefabs[(int)tiles[i, j]]);
+                tilesObject[i, j].transform.position = offset + new Vector3(i * space + space / 2 * (j % 2), 0, j * space * 5.0f / 6.0f) ;
                 tilesObject[i, j].transform.parent = transform;
                 tilesObject[i, j].GetComponent<CaseInfo>().casePos = new Vector2Int(i, j);
             }
