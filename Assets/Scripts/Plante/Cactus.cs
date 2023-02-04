6a8e42459d13057cc4b4f58e552260e2c6c137ed
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-02-04 14:36:55 +0100

    fix typo

--- Assets/Scripts/Plante/Cactus.cs
+++ Assets/Scripts/Plante/Cactus.cs
@@ -9,7 +9,6 @@ public class Cactus : Structure
         productionStats = new int[5] { 0, 0, 0, 0, 0 };
         pvStats = new int[5] { 15, 25, 25, 40, 40 };
         dommageStats = new int[5] { 5, 5, 10, 10, 15 };
-        plantRootRadius = 2;
     }
     public override void ProduceRessource(Player player)
     {
