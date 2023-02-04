a1cad2e022353c7030bc25ab0f9160f7bad1655b
Victorine Cassé <victorine.casse@telecom-sudparis.eu>
2023-02-04 23:51:57 +0100

    ajout du lierre

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,26 +5,10 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {   
-    public AudioClip[] playlist;
+    
     public AudioSource audioSource;
     [SerializeField] AudioMixer audioMixer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSource.clip = playlist[0];
-        audioSource.Play();
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!audioSource.isPlaying)
-        {
-            audioSource.Play();
-        }
-    }
-
+    
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("GlobalVolume", volume);
