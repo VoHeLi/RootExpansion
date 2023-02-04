853393426856f5d4a4c4c5f33a33c22d5a3ff6d4
ThomasSchneider-94 <traschneider@gmail.com>
2023-02-04 15:31:57 +0100

    Sélection des graines (normalement)

--- Assets/Scripts/SelectSeed.cs
+++ Assets/Scripts/SelectSeed.cs
@@ -6,6 +6,12 @@ using TMPro;
 public class SelectSeed : MonoBehaviour
 {
     public TileSelector tileSelector;
+    public TMP_Dropdown startDropdown;
+
+    void start()
+    {
+        OnValueChange(startDropdown);
+    }
 
     public void OnValueChange(TMP_Dropdown dropdown)
     {
@@ -13,6 +19,15 @@ public class SelectSeed : MonoBehaviour
         {
             case 0:
                 tileSelector.changeSelectedPlant(MapBase.StructureType.Tournesol);
+                break;
+            case 1:
+                tileSelector.changeSelectedPlant(MapBase.StructureType.Lierre);
+                break;
+            case 2:
+                tileSelector.changeSelectedPlant(MapBase.StructureType.Carnivore);
+                break;
+            case 3:
+                tileSelector.changeSelectedPlant(MapBase.StructureType.Cactus);
                 break;
         }
     }
