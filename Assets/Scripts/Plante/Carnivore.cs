d72d5ec745cf39ea144201c958a4c76a9885da27
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-02-04 21:06:27 +0100

    debut de l'attaque

--- Assets/Scripts/Plante/Carnivore.cs
+++ Assets/Scripts/Plante/Carnivore.cs
@@ -18,10 +18,79 @@ public class Carnivore : Structure
 
     public override void Action(Player player)
     {
-        throw new System.NotImplementedException();
+        List<Structure> Attackable = new();
+
+        for (int iOffset = attackRange * -2; iOffset < attackRange * 2; iOffset++)
+        {
+            for (int jOffset = attackRange * -2; jOffset < attackRange * 2; jOffset++)
+            {
+                if ((position.x + iOffset >= 0) && (position.x + iOffset < map.height) && (position.y + jOffset >= 0) && (position.y + jOffset < map.width))
+                {
+                    // la case doit etre a une distance de la case
+                    if (GetTileDistance(new Vector2Int(position.x + iOffset, position.y + jOffset)) <= attackRange)
+                    {
+                        if (map.structures[position.x + iOffset, position.y + jOffset] != null
+                            && (map.structures[position.x + iOffset, position.y + jOffset].type != MapBase.StructureType.Racine)
+                            && (map.structures[position.x + iOffset, position.y + jOffset].player == map.roundManager.currentPlayer))
+                        {
+                            Attackable.Add(map.structures[position.x + iOffset, position.y + jOffset]);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        List<Structure> Attacked = new();
+        for (int i = 0; i < 1; i++)
+        {
+            Structure plant = findClosest(Attackable);
+            Attacked.Add(plant);
+            Attackable.Remove(plant);
+        }
+
+        /// MTN JUSTE A FAIRE DES DEGATS AUX PLANTES DANS ATTACKED
     }
 
+    Structure findClosest(List<Structure> structures)
+    {
+        List<Structure> Closests = new();
+        int min = 100;
+        foreach (Structure structure in structures)
+        {
+            int dist = GetTileDistance(structure.position);
+            if (dist < min)
+            {
+                min = dist;
+                Closests.Clear();
+                Closests.Add(structure);
+            }
+            else if (dist == min)
+            {
+                Closests.Add(structure);
+            }
+        }
 
+        return Closests[Random.Range(0, Closests.Count)];
+
+    }
+    int GetTileDistance(Vector2Int case2Pos)
+    {
+        int aX1 = casePos.x;
+        int aY1 = casePos.y;
+        int aX2 = case2Pos.x;
+        int aY2 = case2Pos.y;
+        int dx = aX2 - aX1;     // signed deltas
+        int dy = aY2 - aY1;
+        int x = Mathf.Abs(dx);  // absolute deltas
+        int y = Mathf.Abs(dy);
+        // special case if we start on an odd row or if we move into negative x direction
+        if ((dx < 0) ^ ((aY1 & 1) == 1))
+            x = Mathf.Max(0, x - (y + 1) / 2);
+        else
+            x = Mathf.Max(0, x - (y) / 2);
+        return x + y;
+    }
 
     // Start is called before the first frame update
     void Start()
