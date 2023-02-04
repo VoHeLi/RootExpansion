3220125ffa3b9529ed1b952fa6159233949c9429
Victorine Cassé <victorine.casse@telecom-sudparis.eu>
2023-02-04 22:56:19 +0100

    test

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {   
     public AudioClip[] playlist;
     public AudioSource audioSource;
+    [SerializeField] AudioMixer audioMixer;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +23,19 @@ public class AudioManager : MonoBehaviour
         {
             audioSource.Play();
         }
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("GlobalVolume", volume);
+    }
+    public void SetVolumeMusic(float volume_music)
+    {
+        audioMixer.SetFloat("MusicVolume", volume_music);
+    }
+
+    public void SetVolumeEffects(float volume_effects)
+    {
+        audioMixer.SetFloat("EffectsVolume", volume_effects);
     }
 }
