e25ceaf32a2b01e38c495502c83138cf753eced8
VoHeLi <dahrusted@gmail.com>
2023-02-05 14:28:07 +0100

    corriger affichage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
 
     public IEnumerator WaitForAction()
     {
-        
+        updateRessources();
 
         //Wait for action
         while (action == null)
